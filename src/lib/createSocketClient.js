831ef77f0c8fe5b32372b97b8aa5dd541dc15e8d
y/n <domserdonmandigma@gmail.com>
2025-08-06 12:31:12 +0800

    fix: update SOCKET_SERVER_URL to use NEXT_PUBLIC environment variable

--- src/lib/createSocketClient.js
+++ src/lib/createSocketClient.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const SOCKET_SERVER_URL =
-  process.env.SOCKET_SERVER_URL || "http://localhost:3001";
+  process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:3001";
 
 const createSocketClient = () => {
   return io(SOCKET_SERVER_URL, {
