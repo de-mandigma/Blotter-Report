f26cac864d985cee405e5adbcf371243fd658c32
y/n <domserdonmandigma@gmail.com>
2025-08-06 11:54:50 +0800

    feat: define SOCKET_SERVER_URL for socket client connection

--- src/lib/createSocketClient.js
+++ src/lib/createSocketClient.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
+const SOCKET_SERVER_URL =
+  process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:3001";
+
 const createSocketClient = () => {
   return io(SOCKET_SERVER_URL, {
     transports: ["websocket"],
